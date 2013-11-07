d9d7c12596938af0f70143a46115bdc351a4c959
BuckyFullerInst <dev@bfi.org>
2013-11-07 15:19:44 -0500

    score averaging not working for jurors for some reason

--- ZZZ STUFF TO LOOK AT/blahblah.php
+++ ZZZ STUFF TO LOOK AT/blahblah.php
@@ -1,21 +1,38 @@
-<?php
-	$node = menu_get_object();
-	if ($node->type == 'project') {
-		$node_id = $node->nid;
-		$user_id = $data->uid;
-		$rel_ids = relation_query('user', $user_id)
-			->related('node', $node_id)
-			->execute();
-		$rels = entity_load('relation', array_keys($rel_ids));
-		if (count($rels) == 1) {
-			$value = 1;
-			$rel_wrapper = entity_metadata_wrapper('relation', reset($rels)); // Wrapping fetch relation with Entity API wrapper (https://drupal.org/node/1021556)
+$return = FALSE;
 
-			// Tallying up round 1
-			if ($rel_wrapper->field_project_review_jury_auth->value()) {
-				if ($rel_wrapper->field_project_review_r1_in->value()) print '<i class="icon-check-sign"></i>';
-				else print '<i class="icon-check-minus"></i>';
-			} else print '<i class="icon-check-empty"></i>';
-		} else print 'N/A';
-	}
-?>
+if ($node = menu_get_object()) {
+  global $user;
+  if (isset($user->roles[9]) || isset($user->roles[10]) || isset($user->roles[3])) {		// For review team
+
+    $user_rids = relation_query('user', $user->uid)
+      ->related('node', $node->nid)
+      ->execute();
+    $user_rels = entity_load('relation', array_keys($user_rids));
+    
+    if (count($user_rels) == 1) {
+      $user_rel_wrapper = entity_metadata_wrapper('relation', reset($user_rels));
+      $round_node = $node->field_project_round['und'][0]['value'];    // Matching against current round of project node
+      switch ($round_node) {    // Matching current node round to user-authorized rounds
+        case 1:
+        if ($user_rel_wrapper->field_project_review_r1_auth->value()    
+            && !$user_rel_wrapper->field_project_review_r1_in->value()) $return = TRUE;
+          break;
+        case 2:
+          if ($user_rel_wrapper->field_project_review_r2_auth->value()    
+            && !$user_rel_wrapper->field_project_review_r2_in->value()) $return = TRUE;
+          break;
+        case 3:
+          if ($user_rel_wrapper->field_project_review_r3_auth->value()    
+            && !$user_rel_wrapper->field_project_review_r3_in->value()) $return = TRUE;
+          break;
+        case 4:
+          if ($user_rel_wrapper->field_project_review_jury_auth->value()    
+            && !$user_rel_wrapper->field_project_review_jury_in->value()) $return = TRUE;
+          break;
+      }
+    }
+  }
+}
+
+
+return $return;
